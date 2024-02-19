876da86d22df7896fe69c59b5daf240c0c70c8c3
manoj <142380042+saimanojmatta@users.noreply.github.com>
2024-02-19 12:48:32 +0530

    "added pre intstallation"

--- api/index.js
+++ api/index.js
@@ -4,20 +4,25 @@ import dotenv from 'dotenv'
 import userrouter from './routes/user.router.js'
 import authrouter from './routes/auth.router.js'
 import listingrouter from './routes/listing.route.js'
-
 import cookieParser from 'cookie-parser'
+import path from 'path'
 dotenv.config()
 mongoose.connect(process.env.Mongo).then(()=>{
     console.log('Connected to Mongodb!')
 }).catch((err)=>{
     console.log(err)
 })
+const __dirname=path.resolve()
 const app=express()
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/user',userrouter)
 app.use('/api/auth',authrouter)
 app.use('/api/listing',listingrouter)
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"client","dist","index.html"))
+})
 //errorhandler middleware
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode||500
